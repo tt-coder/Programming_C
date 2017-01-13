73368f81f75e7f8d502e2f1abd1b0278978b1791
tt-coder <tokio.tm@gmail.com>
2017-01-13 21:36:44 +0900

    delete samurai(temp)

--- samurai/manager/errors.cpp
+++
@@ -1,3 +0,0 @@
-#include "samurai.hpp"
-
-ErrorReport::ErrorReport(string s): msg(s) {}
--- samurai/players/greedyPlayer.cpp
+++
@@ -1,4 +0,0 @@
-double territoryMerits = 1;
-double selfTerritoryMerits = 0.3;
-double hurtingMerits = 0.1;
-double hidingMerits = 0.1;
